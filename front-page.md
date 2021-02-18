b2ab433cce7db0716bc2af8e97bbc79f4791acc8
Ben Overmyer <ben@overmyer.net>
2021-02-18 08:37:41 -0600

    Convert everything to LaTeX

--- front-page.md
+++
@@ -1,9 +0,0 @@
-\begin{center}
-{\Huge Mysteries of a Broken World\par}
-
-Playtest Copy
-
-Produced on DATE
-\end{center}
-
-\newpage
--- make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i "s/DATE/`date +'%B %e, %Y'`/g" front-page.md
-pandoc --template=mysteries.latex -f markdown -s -o mysteriesofabrokenworld.pdf front-page.md manuscript/*.md metadata.yaml
